05a750b4f72f32ddc7a26f0be9bf3d7f3c8c05f6
hamza-naseer-engr <engr.hamzanaseer075@gmail.com>
2024-08-08 12:03:22 -0700

    Remove extra services

--- src/services/localStorageService.ts
+++ src/services/localStorageService.ts
@@ -1,4 +1,4 @@
-import { Todo, Streak, StreakMap } from "../types";
+import { Todo, StreakMap } from "../types";
 
 export const loadTodos = (): Todo[] => {
   const todos = localStorage.getItem("todos");
@@ -14,22 +14,6 @@ export const loadTodos = (): Todo[] => {
 
 export const saveTodos = (todos: Todo[]) => {
   localStorage.setItem("todos", JSON.stringify(todos));
-};
-
-export const loadStreaks = (): Streak[] => {
-  const streaks = localStorage.getItem("streaks");
-  if (streaks) {
-    try {
-      return JSON.parse(streaks);
-    } catch (error) {
-      return [];
-    }
-  }
-  return [];
-};
-
-export const saveStreaks = (streaks: Streak[]) => {
-  localStorage.setItem("streaks", JSON.stringify(streaks));
 };
 
 export const loadStreakMap = (): StreakMap => {
